9d1eed67d88b6ae2ca2e35b2fe617e5aaeb324ec
Michael DeWitt <michael.dewitt@conehealth.com>
2021-03-16 10:46:52 -0400

    adding

--- R/2-analysis.R
+++ R/2-analysis.R
@@ -1,3 +1,7 @@
+library(logger)
+
+log_appender(appender_file(file = here::here("logs","analysis.log")))
+
 log_info("Starting Analysis")
 Data <- readRDS("data/Data.rds")
 
@@ -8,7 +12,6 @@ log_info("Running Models")
 
 Mods$"Default" <- lm(immigsent ~ agea + female + eduyrs + uempla + hinctnta + lrscale, data=Data)
 Mods$"Default + Noise" <- lm(immigsent ~ agea + female + eduyrs + uempla + hinctnta + lrscale + noise, data=Data)
-
 
 # models completed --------------------------------------------------------
 
--- R/3-sims.R
+++ R/3-sims.R
@@ -2,7 +2,10 @@
 library(stevemisc)
 library(tidyverse)
 library(modelr)
+library(validate)
+library(logger)
 
+log_appender(appender_file(file = here::here("logs","analysis.log")))
 # Load stuff
 Mods <- readRDS("data/Mods.rds")
 Data <- readRDS("data/Data.rds")
@@ -22,4 +25,29 @@ newdat %>%
 
 log_info("Simulations Complete")
 
+
+# validate simulations for craziness ---------------------------------------
+validation_template <- here::here("tests", "model-validation.yml")
+
+v <- validator(.file = validation_template)
+
+confront_data <- confront(Sims$`SQI (Ideology)`, v)
+
+confront_results <- summary(confront_data)
+
+for(i in 1:nrow(confront_results)){
+  if(confront_results[i,]$fails>0){
+    log_info(glue::glue_data(.x = confront_results[i,],
+                             "{name}; passed = {passes}; failures = {fails}"))
+  } else {
+    log_error(glue::glue_data(.x = confront_results[i,],
+                              "{name}; passed = {passes}; failures = {fails}"))
+  }
+
+}
+
+
+# save output -------------------------------------------------------------
+
+log_info("Simulations Saved")
 saveRDS(Sims, "data/Sims.rds")
